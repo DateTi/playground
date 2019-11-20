8af96ee9d2fb62e9dd4c156fc8ecb86dd82ac653
Jan Galek <admin@gcore.cz>
2019-11-20 09:58:50 +0100

    Update

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -6,9 +6,12 @@ namespace App\Presenters;
 
 use DateTi\DateTi;
 use Nette;
+use stdClass;
 
-
-final class HomepagePresenter extends Nette\Application\UI\Presenter
+/**
+ * @property-read Nette\Bridges\ApplicationLatte\Template|stdClass $template
+ */
+class HomepagePresenter extends Nette\Application\UI\Presenter
 {
 
     /** @var DateTi */
@@ -19,8 +22,9 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
         $this->dateTi = $dateTi;
     }
 
-    public function renderDefault()
+    public function renderDefault(): void
     {
         $this->template->date = $this->dateTi->format('d.m.Y');
+
     }
 }
